6af20c79b2f70dfd059abc2c401cf111cadf52cc
Franklin Davenport <sparebytes@gmail.com>
2020-02-07 09:17:18 -0600

    fix: type declaration of close

--- src/fsp.ts
+++ src/fsp.ts
@@ -3,45 +3,47 @@ import { promisify } from "util";
 const promises = fs.promises;
 
 function maybePromisify<T extends Function>(fn: T | undefined | null) {
-    if (fn == null) {
-        return fn;
-    }
-    return promisify(fn);
+  if (fn == null) {
+    return fn;
+  }
+  return promisify(fn);
 }
-export const access = promises.access ?? promisify(fs.access);
-export const appendFile = promises.appendFile ?? promisify(fs.appendFile);
-export const chmod = promises.chmod ?? promisify(fs.chmod);
-export const chown = promises.chown ?? promisify(fs.chown);
-export const link = promises.link ?? promisify(fs.link);
-export const mkdir = promises.mkdir ?? promisify(fs.mkdir);
-export const mkdtemp = promises.mkdtemp ?? promisify(fs.mkdtemp);
-export const open = promises.open ?? promisify(fs.open);
-export const read = promises.read ?? promisify(fs.read);
-export const readdir = promises.readdir ?? promisify(fs.readdir);
-export const readFile = promises.readFile ?? promisify(fs.readFile);
-export const readlink = promises.readlink ?? promisify(fs.readlink);
-export const realpath = promises.realpath ?? promisify(fs.realpath);
-export const rename = promises.rename ?? promisify(fs.rename);
-export const rmdir = promises.rmdir ?? promisify(fs.rmdir);
-export const stat = promises.stat ?? promisify(fs.stat);
-export const symlink = promises.symlink ?? promisify(fs.symlink);
-export const truncate = promises.truncate ?? promisify(fs.truncate);
-export const unlink = promises.unlink ?? promisify(fs.unlink);
-export const utimes = promises.utimes ?? promisify(fs.utimes);
-export const write = promises.write ?? promisify(fs.write);
-export const writeFile = promises.writeFile ?? promisify(fs.writeFile);
+
+// functions that might have built-in promisified functions already
+export const access: typeof promises.access = promises.access ?? promisify(fs.access);
+export const appendFile: typeof promises.appendFile = promises.appendFile ?? promisify(fs.appendFile);
+export const chmod: typeof promises.chmod = promises.chmod ?? promisify(fs.chmod);
+export const chown: typeof promises.chown = promises.chown ?? promisify(fs.chown);
+export const link: typeof promises.link = promises.link ?? promisify(fs.link);
+export const mkdir: typeof promises.mkdir = promises.mkdir ?? promisify(fs.mkdir);
+export const mkdtemp: typeof promises.mkdtemp = promises.mkdtemp ?? promisify(fs.mkdtemp);
+export const open: typeof promises.open = promises.open ?? promisify(fs.open);
+export const read: typeof promises.read = promises.read ?? promisify(fs.read);
+export const readdir: typeof promises.readdir = promises.readdir ?? promisify(fs.readdir);
+export const readFile: typeof promises.readFile = promises.readFile ?? promisify(fs.readFile);
+export const readlink: typeof promises.readlink = promises.readlink ?? promisify(fs.readlink);
+export const realpath: typeof promises.realpath = promises.realpath ?? promisify(fs.realpath);
+export const rename: typeof promises.rename = promises.rename ?? promisify(fs.rename);
+export const rmdir: typeof promises.rmdir = promises.rmdir ?? promisify(fs.rmdir);
+export const stat: typeof promises.stat = promises.stat ?? promisify(fs.stat);
+export const symlink: typeof promises.symlink = promises.symlink ?? promisify(fs.symlink);
+export const truncate: typeof promises.truncate = promises.truncate ?? promisify(fs.truncate);
+export const unlink: typeof promises.unlink = promises.unlink ?? promisify(fs.unlink);
+export const utimes: typeof promises.utimes = promises.utimes ?? promisify(fs.utimes);
+export const write: typeof promises.write = promises.write ?? promisify(fs.write);
+export const writeFile: typeof promises.writeFile = promises.writeFile ?? promisify(fs.writeFile);
 
 // functions that don't have built-in promisified functions already
-export const close = (promises as any).close ?? promisify(fs.close);
+export const close: (fd: number) => Promise<void> = (promises as any).close ?? promisify(fs.close);
 
 // functions that may not be available on every operating system
-export const fchmod = promises.fchmod ?? maybePromisify(fs.fchmod);
-export const fchown = promises.fchown ?? maybePromisify(fs.fchown);
-export const fdatasync = promises.fdatasync ?? maybePromisify(fs.fdatasync);
-export const fstat = promises.fstat ?? maybePromisify(fs.fstat);
-export const fsync = promises.fsync ?? maybePromisify(fs.fsync);
-export const ftruncate = promises.ftruncate ?? maybePromisify(fs.ftruncate);
-export const futimes = promises.futimes ?? maybePromisify(fs.futimes);
-export const lchmod = promises.lchmod ?? maybePromisify(fs.lchmod);
-export const lchown = promises.lchown ?? maybePromisify(fs.lchown);
-export const lstat = promises.lstat ?? maybePromisify(fs.lstat);
+export const fchmod: typeof promises.fchmod | undefined = promises.fchmod ?? maybePromisify(fs.fchmod);
+export const fchown: typeof promises.fchown | undefined = promises.fchown ?? maybePromisify(fs.fchown);
+export const fdatasync: typeof promises.fdatasync | undefined = promises.fdatasync ?? maybePromisify(fs.fdatasync);
+export const fstat: typeof promises.fstat | undefined = promises.fstat ?? maybePromisify(fs.fstat);
+export const fsync: typeof promises.fsync | undefined = promises.fsync ?? maybePromisify(fs.fsync);
+export const ftruncate: typeof promises.ftruncate | undefined = promises.ftruncate ?? maybePromisify(fs.ftruncate);
+export const futimes: typeof promises.futimes | undefined = promises.futimes ?? maybePromisify(fs.futimes);
+export const lchmod: typeof promises.lchmod | undefined = promises.lchmod ?? maybePromisify(fs.lchmod);
+export const lchown: typeof promises.lchown | undefined = promises.lchown ?? maybePromisify(fs.lchown);
+export const lstat: typeof promises.lstat | undefined = promises.lstat ?? maybePromisify(fs.lstat);
